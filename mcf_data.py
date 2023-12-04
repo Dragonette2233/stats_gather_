76843e7dc512d0fa1ee49b33f5da244a52e77eff
unknown <rodanban251@gmail.com>
2023-12-05 00:16:19 +0300

    savepoint

--- mcf_data.py
+++ mcf_data.py
@@ -10,7 +10,7 @@ SET_LCK = threading.Lock()
 GATHERED_MATCHES = set()
 
 headers_timeout = {
-            'headers': { "X-Riot-Token": open('api.key', 'r').read() },
+            'headers': { "X-Riot-Token": open('apikey', 'r').read() },
             'timeout': 3
         }
 FEATURED_GAMES_URL = "https://{region}.api.riotgames.com/lol/spectator/v4/featured-games"
